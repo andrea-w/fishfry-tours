e5482aefcf14d0e706eb1e123781524663c695a3
Andrea Williams <andrea.k.williams7@gmail.com>
2022-01-30 18:01:47 -0800

    Added DATABASE_URL var in base.py

--- fishfry/fishfry/base.py
+++ fishfry/fishfry/base.py
@@ -3,7 +3,8 @@ import typing
 
 
 class Config:
-    DB_URI: str = "postgresql+asyncpg://pgadmin:password@localhost/testdb"
+    # DB_URI: str = "postgresql+asyncpg://pgadmin:password@localhost/testdb"
+    DB_URI: str = os.environ['DATABASE_URL']
 
     def __init__(self, prefix: str = "FISHFRY_"):
         for name, type_ in typing.get_type_hints(self).items():
